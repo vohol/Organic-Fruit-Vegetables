b281a65e759617a73f5ff0098b3fe64f324d0626
Kholiavskiy Vladimir <vohol@pop-os.localdomain>
2022-03-25 14:14:30 +0200

    done header

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,20 @@
 import * as flsFunctions from "./modules/functions.js";
-
 flsFunctions.isWebp();
 
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+const headerBurger = document.querySelector('.header__burger')
+const headerNavigation = document.querySelector('.header__navigation')
+const headerLogo = document.querySelector('.header__logo')
+const headerRegBtn = document.querySelector('.header__reg-btn')
+
+document.addEventListener('click', openMobileMenu);
+
+function openMobileMenu (event) {
+  const target = event.target;
+
+  if (target.closest('.header__burger-wrapper')) {
+    headerBurger.classList.toggle('active')
+    headerNavigation.classList.toggle('active')
+    headerLogo.classList.toggle('active')
+    headerRegBtn.classList.toggle('mobile')
+  }
+}
