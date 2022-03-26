05416097f429cdcf716ab1af0cee2254e8d5705c
Kholiavskiy Vladimir <vohol@pop-os.localdomain>
2022-03-26 22:42:50 +0200

    hero: added paralax and animation counters

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
+const scene = document.getElementById('scene');
+const parallax = new Parallax(scene);
+
 const headerBurger = document.querySelector('.header__burger')
 const headerNavigation = document.querySelector('.header__navigation')
 const headerLogo = document.querySelector('.header__logo')
@@ -29,4 +32,57 @@ function openMobileMenu (event) {
     headerLogo.classList.remove('active')
     headerRegBtn.classList.remove('mobile')
   }
+}
+
+const counters = document.querySelectorAll('.hero__counter-item')
+const reviewItems = document.querySelectorAll('.hero__review-item')
+
+
+window.addEventListener('load', addOpacity)
+
+function addOpacity() {
+  reviewItems.forEach(element => {
+    element.classList.add('not-opacity')
+    
+  });
+}
+
+window.addEventListener('load', handlerCounters)
+
+function handlerCounters(event) {
+
+  counters.forEach(element => {
+    if (element.classList.contains('order-user')) {
+      counter(element, 30)
+    }
+    if (element.classList.contains('reviews')) {
+      counter(element, 20)
+    }
+    if (element.classList.contains('items')) {
+      counter(element, 300, 50)
+    }
+  });
+
+}
+
+function counter(element, max, interval=100) {
+  let counter = 0;
+  let countFunction = setInterval(function () {
+    max > 100 ? counter +=10 : counter +=2
+    element.textContent = counter
+
+    if (counter == max * 0.8) {
+      clearInterval(countFunction)
+
+      let slowerCountFunction = setInterval(function () {
+        max > 100 ? counter +=5 : counter ++
+        element.textContent = counter
+
+        if (counter >= max ) {
+          clearInterval(slowerCountFunction)
+
+        }
+      }, interval * 2)
+    }
+  }, interval)
 }
