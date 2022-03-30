2a4e16d26130427a9a9a46d1f65e3e429b55e65f
Kholiavskiy Vladimir <vohol@pop-os.localdomain>
2022-03-30 16:20:26 +0300

    done download-app section

--- gulpfile.js
+++ gulpfile.js
@@ -40,9 +40,7 @@ function watcher() {
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 // Основные задачи
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images,
-	 // svgSpriteTask
-	 ));
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images, svgSpriteTask ));
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
